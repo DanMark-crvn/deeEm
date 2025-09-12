2bae295b4dcbfc013d4deb425b2fe04f0168aa9b
unknown <caravanadanmark@gmail.com>
2025-09-13 01:16:28 +0800

    few changes in appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './header'
+import Header from './Header'
 import Footer from './Footer'
 import MainContent from './MainContent'
 import Details from "./details.json";
